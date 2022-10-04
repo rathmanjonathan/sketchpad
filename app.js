7df473f9e32a15daf4fe02ec695693159d13ee75
jonathan <rathman.jonathan@gmail.com>
2022-10-04 11:24:31 -0700

    able to draw on sketchpad

--- app.js
+++ app.js
@@ -1,8 +1,36 @@
-let board = document.querySelector('.board')
-board.style.gridTemplateColumns = 'repeat(16, 1fr)'
-board.style.gridTemplateRows = 'repeat(16, 1fr)'
+function populateBoard(size) {
+let board = document.querySelector('.board');
 
-let buttons = document.querySelectorAll('.buttons')
+board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+let numDivs = size * size;
+
+changeSize();
+
+for (let i = 0; i < numDivs; i++) {
+    let div = document.createElement('div');
+    board.insertAdjacentElement('beforeend', div);
+    div.style.backgroundColor = 'blue';
+    div.addEventListener('mouseover', () => {
+        div.style.backgroundColor = 'black';
+    })
+    
+    }
+}
+populateBoard(16);
+
+function changeSize(size) {
+    let userInput = prompt('Adjust board size');
+}
+
+function randomColor() {
+    const btn_randomColor = document.querySelector('.random');
+    btn_randomColor.addEventListener('click', (e) => {
+        console.log(btn_randomColor);
+    })
+}
+
 
 
 
